5fa8936a6b76d9e08ae97eaba18996ddf342a26f
Yuriy Gaiduk <gaydukyuriy2@gmail.com>
2019-07-22 11:48:35 +0300

    Изменил alt для картинок,центрировал counter в футере

--- js/script.js
+++ js/script.js
@@ -46,14 +46,3 @@ $('.slider2 .owl-carousel').owlCarousel({
 
 
 });
-const iconSearch = document.querySelector('.icons__search');
-const body = document.querySelector('body');
-
-iconSearch.addEventListener('click', function(event){
-  event.stopPropagation();
-  iconSearch.classList.add('icons__search--active');
-});
-
-body.addEventListener('click', function(event){
-  iconSearch.classList.remove('icons__search--active');
-});
